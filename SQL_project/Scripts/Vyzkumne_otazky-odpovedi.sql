f523e81c0640971929ee2d5747ef7811cdd9985c
PavelK48 <pavel_kozak@centrum.cz>
2024-11-03 21:32:54 +0100

    Zodpovězení otázky č. 2

--- SQL_project/Scripts/Vyzkumne_otazky-odpovedi.sql
+++ SQL_project/Scripts/Vyzkumne_otazky-odpovedi.sql
@@ -24,10 +24,18 @@ WHERE diff < 0
 V případě zadání opačného znaménka ">" nám výsledek ukáže roky a odvětví,
 ve kterých mzda rostla. */
 
--- 2 Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+
+
+-- 2. Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
  
 -- pomocný výpočet
 /*
+SELECT *
+FROM t_pavel_kozak_project_sql_primary_final tpk
+WHERE `year` IN (2006,2018)	
+	AND category_name IS NULL
+;
+
 SELECT *
 FROM t_pavel_kozak_project_sql_primary_final tpk
 WHERE `year` IN (2006,2018)
@@ -35,10 +43,36 @@ WHERE `year` IN (2006,2018)
 	OR category_name IS NULL
 	AND `year` IN (2006,2018)
 ;
--- pomocný výpočet
-SELECT *
-FROM t_pavel_kozak_project_sql_primary_final tpk
-WHERE `year` IN (2006,2018)	
-	AND category_name IS NULL; */
+*/
 
 --  řešení:
+SELECT 
+	tpk.`year`
+	, tpk.category_name
+	, ROUND (tpk2.value/tpk.value, 2) AS result
+FROM t_pavel_kozak_project_sql_primary_final tpk
+JOIN t_pavel_kozak_project_sql_primary_final tpk2
+ON tpk.`year`=tpk2.`year`
+	AND tpk.`year` IN (2006,2018)
+	AND tpk.code IN ('114201', '111301', 'NULL')
+	AND tpk2.category_name IS NULL
+;
+
+-- Výsledné řešení ukazuje sloupeček "result", který nám říká kolik litrů mléka a kilogramů chleba si můžeme v letech 2006 a 2018 koupit.
+
+-- Varianta řešení č.2 za pomocí vnořeného selectu, ovšem není uplně přehledné. Mohlo by sloužit k případnému porovnání mezi těmito roky.
+ SELECT `year`
+	, category_name
+	,value	
+	,ROUND ((SELECT value
+	  FROM t_pavel_kozak_project_sql_primary_final tpk
+	  WHERE `year` = 2006	
+			AND category_name IS NULL)/value, 2) AS avg_2006
+	,ROUND ((SELECT value
+	  FROM t_pavel_kozak_project_sql_primary_final tpk
+	  WHERE `year` = 2018	
+			AND category_name IS NULL)/value, 2) AS avg_2018
+FROM t_pavel_kozak_project_sql_primary_final tpk
+WHERE `year` IN (2006,2018)
+	AND code IN ('114201', '111301')
+;
